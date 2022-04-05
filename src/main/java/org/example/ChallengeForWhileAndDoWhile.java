16f11c06aee41ecdaa3652eafdc00c0d87fddd10
NegarMadadi <madadi.negar@gmail.com>
2022-04-05 14:53:29 +0200

    Created ChallengeForWhileAndDoWhile

---
+++ src/main/java/org/example/ChallengeForWhileAndDoWhile.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class ChallengeForWhileAndDoWhile {
+    public static void main(String[] args) {
+
+        int number = 4;
+        int finishNumber = 20;
+        int evenNumberFound = 0;
+
+        while (number <= finishNumber) {
+            number++;
+            if (!isEvenNumber(number)) {
+                continue;
+            }
+            System.out.println("Even number " + number);
+            evenNumberFound++;
+            if (evenNumberFound >= 5) {
+                break;
+            }
+        }
+        System.out.println("Total even numbers found = " + evenNumberFound);
+    }
+
+       /* while(number <= finishNumber){
+            number++;
+            if (!isEvenNumber(number)){
+                continue;
+            }
+            System.out.println("Even number " + number);
+        }*/
+
+    // Modify the while above code
+    //Make it also record the total number of even numbers it has found
+    //and break since 5 are found
+    // and at the end, display the total number of even numbers.
+
+    public static boolean isEvenNumber(int number) {
+        return number % 2 == 0;
+    }
+}
