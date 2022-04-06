7c55415db19b5a05c1ef6fec3405e30331dc50cd
NegarMadadi <madadi.negar@gmail.com>
2022-04-06 07:53:50 +0200

    Created EvenDigitSum.java

--- src/main/java/org/example/EvenDigitSum.java
+++ src/main/java/org/example/EvenDigitSum.java
@@ -25,10 +25,9 @@ public class EvenDigitSum {
         System.out.println(getEvenDigitSum(123456789)); //→ should return 20 since 2 + 4 + 6 + 8 = 20
         System.out.println(getEvenDigitSum(252)); //→ should return 4 since 2 + 2 = 4
         System.out.println(getEvenDigitSum(-22)); //→ should return -1 since the number is negative
-
     }
 
-    private static int getEvenDigitSum(int number) {
+    public static int getEvenDigitSum(int number) {
         if (number < 0) {
             return -1;
         }
