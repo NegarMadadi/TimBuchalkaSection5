11829de51a4bf539f2a2993a4578906ab3d55741
NegarMadadi <madadi.negar@gmail.com>
2022-04-09 23:08:31 +0200

    Created Person.java

---
+++ src/main/java/org/example/section6/Person.java
@@ -0,0 +1,57 @@
+package org.example.section6;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setAge(int age) {
+        if (age >= 0 && age <= 100) {
+            this.age = age;
+        } else {
+            this.age = 0;
+        }
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public boolean isTeen() {
+        return (age > 12 && age < 20);
+    }
+
+    public String getFullName() {
+        if (firstName.isEmpty() && lastName.isEmpty()) {
+            return "";
+        }
+        if (lastName.isEmpty()) {
+            return firstName;
+        }
+        if (firstName.isEmpty()) {
+            return lastName;
+        }
+        return firstName + " " + lastName;
+    }
+
+
+
+
+}
